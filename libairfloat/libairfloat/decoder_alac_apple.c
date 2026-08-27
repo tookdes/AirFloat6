47091a1ff96b6e1cd6f26ba41aabca51fbe2a501
tookdes <38042623+tookdes@users.noreply.github.com>
2026-08-28 00:27:32 +0800

    fix: fail ALAC decoder safely on invalid input

--- libairfloat/libairfloat/decoder_alac_apple.c
+++ libairfloat/libairfloat/decoder_alac_apple.c
@@ -30,7 +30,10 @@
 
 #if (defined(__APPLE__) && !defined(ALAC_SOFTWARE))
 
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <limits.h>
 
 #include <AudioToolbox/AudioToolbox.h>
 
@@ -40,7 +43,6 @@
 #include "alac_format.h"
 
 #define MIN(x, y) (x < y ? x : y)
-#define ca_assert(error) assert((error) == noErr)
 
 struct decoder_alac_mac_t {
     struct alac_magic_cookie_t magic_cookie;
@@ -48,118 +50,189 @@ struct decoder_alac_mac_t {
     AudioStreamBasicDescription out_desc;
     AudioConverterRef converter_ref;
     AudioStreamPacketDescription packet_description;
-    AudioBuffer audio_buffer;
     void* buffer_p;
     size_t buffer_size;
 };
 
+static bool _decoder_alac_config_is_valid(struct alac_specific_config_t config) {
+    uint32_t frame_length = btml(config.frame_length);
+    uint32_t sample_rate = btml(config.sample_rate);
+    return (frame_length > 0 && sample_rate > 0 && config.num_channels > 0 &&
+            config.bit_depth >= 8 && config.bit_depth <= 32 && (config.bit_depth % 8) == 0);
+}
+
+static void _decoder_alac_cleanup(struct decoder_alac_mac_t* d) {
+    if (d == NULL)
+        return;
+    if (d->converter_ref != NULL) {
+        OSStatus status = AudioConverterDispose(d->converter_ref);
+        if (status != noErr)
+            log_message(LOG_ERROR, "Unable to dispose ALAC converter (%d)", (int)status);
+        d->converter_ref = NULL;
+    }
+}
+
 void* decoder_alac_create(const char* rtp_fmtp) {
     
+    if (rtp_fmtp == NULL)
+        return NULL;
+    
     struct decoder_alac_mac_t* d = (struct decoder_alac_mac_t*)malloc(sizeof(struct decoder_alac_mac_t));
+    if (d == NULL)
+        return NULL;
     bzero(d, sizeof(struct decoder_alac_mac_t));
     
     d->magic_cookie = alac_format_parse(rtp_fmtp);
-    
     struct alac_specific_config_t config = d->magic_cookie.alac_specific_info.config;
+    if (!_decoder_alac_config_is_valid(config)) {
+        free(d);
+        return NULL;
+    }
     
-    AudioStreamBasicDescription in_desc;
-    bzero(&in_desc, sizeof(AudioStreamBasicDescription));
+    struct AudioStreamBasicDescription in_desc;
+    bzero(&in_desc, sizeof(in_desc));
     
     in_desc.mFormatID = kAudioFormatAppleLossless;
     in_desc.mSampleRate = btml(config.sample_rate);
-    in_desc.mFramesPerPacket = d->output_format.frames_per_packet = btml(config.frame_length);
+    in_desc.mFramesPerPacket = btml(config.frame_length);
     in_desc.mChannelsPerFrame = config.num_channels;
     
     d->out_desc.mFormatID = kAudioFormatLinearPCM;
     d->out_desc.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
-    d->out_desc.mSampleRate = d->output_format.sample_rate = btml(config.sample_rate);
-    d->out_desc.mChannelsPerFrame = d->output_format.channels = config.num_channels;
+    d->out_desc.mSampleRate = in_desc.mSampleRate;
+    d->out_desc.mChannelsPerFrame = config.num_channels;
     d->out_desc.mFramesPerPacket = 1;
-    d->out_desc.mBitsPerChannel = d->output_format.bit_depth = config.bit_depth;
+    d->out_desc.mBitsPerChannel = config.bit_depth;
     
-    d->output_format.frame_size = d->output_format.channels * (d->output_format.bit_depth / 8);
-    
-    /* Make Core Audio fill out rest of outDesc. */
     UInt32 size = sizeof(AudioStreamBasicDescription);
-    ca_assert(AudioFormatGetProperty(kAudioFormatProperty_FormatInfo, 0, NULL, &size, &d->out_desc));
+    OSStatus status = AudioFormatGetProperty(kAudioFormatProperty_FormatInfo, 0, NULL, &size, &d->out_desc);
+    if (status != noErr || d->out_desc.mBytesPerFrame == 0) {
+        log_message(LOG_ERROR, "Invalid ALAC output format (%d)", (int)status);
+        free(d);
+        return NULL;
+    }
     
-    ca_assert(AudioConverterNew(&in_desc, &d->out_desc, &d->converter_ref));
+    d->output_format.frames_per_packet = in_desc.mFramesPerPacket;
+    d->output_format.sample_rate = (uint32_t)d->out_desc.mSampleRate;
+    d->output_format.channels = d->out_desc.mChannelsPerFrame;
+    d->output_format.bit_depth = d->out_desc.mBitsPerChannel;
+    d->output_format.frame_size = d->out_desc.mBytesPerFrame;
     
-    AudioChannelLayout channelLayout;
-    bzero(&channelLayout, sizeof(channelLayout));
-    channelLayout.mChannelLayoutTag = (config.num_channels == 2 ? kAudioChannelLayoutTag_Stereo : kAudioChannelLayoutTag_Mono);
+    status = AudioConverterNew(&in_desc, &d->out_desc, &d->converter_ref);
+    if (status != noErr || d->converter_ref == NULL) {
+        log_message(LOG_ERROR, "Unable to create ALAC converter (%d)", (int)status);
+        free(d);
+        return NULL;
+    }
     
-    ca_assert(AudioConverterSetProperty(d->converter_ref, kAudioConverterDecompressionMagicCookie, sizeof(struct alac_magic_cookie_t), &d->magic_cookie));
-    ca_assert(AudioConverterSetProperty(d->converter_ref, kAudioConverterInputChannelLayout, sizeof(channelLayout), &channelLayout));
+    AudioChannelLayout channel_layout;
+    bzero(&channel_layout, sizeof(channel_layout));
+    channel_layout.mChannelLayoutTag = (config.num_channels == 2 ? kAudioChannelLayoutTag_Stereo : kAudioChannelLayoutTag_Mono);
+    
+    status = AudioConverterSetProperty(d->converter_ref, kAudioConverterDecompressionMagicCookie, sizeof(struct alac_magic_cookie_t), &d->magic_cookie);
+    if (status == noErr)
+        status = AudioConverterSetProperty(d->converter_ref, kAudioConverterInputChannelLayout, sizeof(channel_layout), &channel_layout);
+    
+    if (status != noErr) {
+        log_message(LOG_ERROR, "Unable to configure ALAC converter (%d)", (int)status);
+        _decoder_alac_cleanup(d);
+        free(d);
+        return NULL;
+    }
     
     return d;
-    
 }
 
 void decoder_alac_destroy(void* data) {
     
     struct decoder_alac_mac_t* d = (struct decoder_alac_mac_t*)data;
+    if (d == NULL)
+        return;
     
-    ca_assert(AudioConverterDispose(d->converter_ref));
-    
+    _decoder_alac_cleanup(d);
     free(d);
-    
 }
 
 struct decoder_output_format_t decoder_alac_get_output_format(void* data) {
     
-    return ((struct decoder_alac_mac_t*)data)->output_format;
-    
+    struct decoder_alac_mac_t* d = (struct decoder_alac_mac_t*)data;
+    return d != NULL ? d->output_format : (struct decoder_output_format_t){ 0, 0, 0, 0 };
 }
 
-OSStatus _decoder_alac_mac_input_data_proc(AudioConverterRef inAudioConverter, UInt32 *ioNumberDataPackets, AudioBufferList *ioData, AudioStreamPacketDescription  **outDataPacketDescription, void *inUserData) {
+OSStatus _decoder_alac_mac_input_data_proc(AudioConverterRef inAudioConverter, UInt32 *ioNumberDataPackets, AudioBufferList *ioData, AudioStreamPacketDescription **outDataPacketDescription, void *inUserData) {
     
     struct decoder_alac_mac_t* d = (struct decoder_alac_mac_t*)inUserData;
+    if (d == NULL || ioNumberDataPackets == NULL || ioData == NULL || d->buffer_p == NULL || d->buffer_size == 0 || d->buffer_size > UINT32_MAX) {
+        if (ioNumberDataPackets != NULL)
+            *ioNumberDataPackets = 0;
+        return kAudio_ParamError;
+    }
     
-    (*ioData).mBuffers[0].mData = d->buffer_p;
-    (*ioData).mBuffers[0].mDataByteSize = (UInt32)d->buffer_size;
-    (*ioData).mBuffers[0].mNumberChannels = d->magic_cookie.alac_specific_info.config.num_channels;
+    *ioNumberDataPackets = 1;
+    ioData->mNumberBuffers = 1;
+    ioData->mBuffers[0].mData = d->buffer_p;
+    ioData->mBuffers[0].mDataByteSize = (UInt32)d->buffer_size;
+    ioData->mBuffers[0].mNumberChannels = d->magic_cookie.alac_specific_info.config.num_channels;
     
+    bzero(&d->packet_description, sizeof(d->packet_description));
     d->packet_description.mDataByteSize = (UInt32)d->buffer_size;
     
-    if (outDataPacketDescription)
+    if (outDataPacketDescription != NULL)
         *outDataPacketDescription = &d->packet_description;
     
     return noErr;
-    
 }
 
 size_t decoder_alac_decode(void* data, void* in_audio_data, size_t in_audio_data_size, void* out_audio_data, size_t out_audio_data_size) {
     
     struct decoder_alac_mac_t* d = (struct decoder_alac_mac_t*)data;
-    
-    assert(in_audio_data != NULL && out_audio_data != NULL && out_audio_data_size > 0);
-    
-    d->audio_buffer.mData = in_audio_data;
-    d->audio_buffer.mDataByteSize = (UInt32)in_audio_data_size;
-    d->audio_buffer.mNumberChannels = d->magic_cookie.alac_specific_info.config.num_channels;
+    if (d == NULL || d->converter_ref == NULL || in_audio_data == NULL || out_audio_data == NULL ||
+        in_audio_data_size == 0 || out_audio_data_size == 0 ||
+        in_audio_data_size > UINT32_MAX || out_audio_data_size > UINT32_MAX ||
+        d->output_format.frame_size == 0)
+        return 0;
     
     AudioBufferList out_buffer_list;
+    bzero(&out_buffer_list, sizeof(out_buffer_list));
     out_buffer_list.mNumberBuffers = 1;
     out_buffer_list.mBuffers[0].mData = out_audio_data;
     out_buffer_list.mBuffers[0].mDataByteSize = (UInt32)out_audio_data_size;
+    out_buffer_list.mBuffers[0].mNumberChannels = d->output_format.channels;
     
     d->buffer_size = in_audio_data_size;
     d->buffer_p = in_audio_data;
     
-    UInt32 io_output_data_packets = (UInt32)out_audio_data_size / d->output_format.frame_size;
-    ca_assert(AudioConverterFillComplexBuffer(d->converter_ref, _decoder_alac_mac_input_data_proc, d, &io_output_data_packets, &out_buffer_list, NULL));
+    UInt32 io_output_data_packets = (UInt32)(out_audio_data_size / d->output_format.frame_size);
+    if (io_output_data_packets == 0) {
+        d->buffer_p = NULL;
+        d->buffer_size = 0;
+        return 0;
+    }
     
-    return d->output_format.frames_per_packet * d->out_desc.mBytesPerFrame;
+    OSStatus status = AudioConverterFillComplexBuffer(d->converter_ref, _decoder_alac_mac_input_data_proc, d, &io_output_data_packets, &out_buffer_list, NULL);
+    d->buffer_p = NULL;
+    d->buffer_size = 0;
     
+    if (status != noErr) {
+        log_message(LOG_ERROR, "ALAC decode failed (%d)", (int)status);
+        return 0;
+    }
+    
+    if (io_output_data_packets > SIZE_MAX / d->output_format.frame_size)
+        return 0;
+    size_t decoded_size = (size_t)io_output_data_packets * d->output_format.frame_size;
+    return MIN(decoded_size, out_audio_data_size);
 }
 
 void decoder_alac_reset(void* data) {
     
     struct decoder_alac_mac_t* d = (struct decoder_alac_mac_t*)data;
+    if (d == NULL || d->converter_ref == NULL)
+        return;
     
-    ca_assert(AudioConverterReset(d->converter_ref));
-    
+    OSStatus status = AudioConverterReset(d->converter_ref);
+    if (status != noErr)
+        log_message(LOG_ERROR, "Unable to reset ALAC converter (%d)", (int)status);
 }
 
 #endif
