8e38781c468ad572434723830cda16b4de8211ca
tookdes <38042623+tookdes@users.noreply.github.com>
2026-08-28 00:40:43 +0800

    fix: make socket address helpers null-safe

--- libairfloat/libairfloat/sockaddr.c
+++ libairfloat/libairfloat/sockaddr.c
@@ -36,25 +36,31 @@
 
 struct sockaddr* sockaddr_create(const char* host, uint16_t port, sockaddr_type version, uint32_t scope_id) {
     
-    if (!version) version = sockaddr_type_inet_4;
+    if (!version)
+        version = sockaddr_type_inet_4;
     
     if (version == sockaddr_type_inet_4) {
-        
-        struct sockaddr_in* ret = (struct sockaddr_in*)malloc(sizeof(struct sockaddr_storage));
-        bzero(ret, sizeof(struct sockaddr_storage));
+        struct sockaddr_in* ret = (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
+        if (ret == NULL)
+            return NULL;
+        bzero(ret, sizeof(struct sockaddr_in));
         ret->sin_len = sizeof(struct sockaddr_in);
         ret->sin_family = AF_INET;
         ret->sin_addr.s_addr = INADDR_ANY;
         ret->sin_port = htons(port);
         
-        if (host != NULL)
-            inet_pton(AF_INET, host, &ret->sin_addr);
+        if (host != NULL && inet_pton(AF_INET, host, &ret->sin_addr) != 1) {
+            free(ret);
+            return NULL;
+        }
         
         return (struct sockaddr*)ret;
-        
-    } else if (version == sockaddr_type_inet_6) {
-        
+    }
+    
+    if (version == sockaddr_type_inet_6) {
         struct sockaddr_in6* ret = (struct sockaddr_in6*)malloc(sizeof(struct sockaddr_in6));
+        if (ret == NULL)
+            return NULL;
         bzero(ret, sizeof(struct sockaddr_in6));
         ret->sin6_len = sizeof(struct sockaddr_in6);
         ret->sin6_family = AF_INET6;
@@ -62,91 +68,95 @@ struct sockaddr* sockaddr_create(const char* host, uint16_t port, sockaddr_type 
         ret->sin6_port = htons(port);
         ret->sin6_scope_id = scope_id;
         
-        if (host != NULL)
-            inet_pton(AF_INET6, host, &ret->sin6_addr);
+        if (host != NULL && inet_pton(AF_INET6, host, &ret->sin6_addr) != 1) {
+            free(ret);
+            return NULL;
+        }
         
         return (struct sockaddr*)ret;
-        
     }
     
     return NULL;
-    
 }
 
 void sockaddr_destroy(struct sockaddr* addr) {
-    
     free(addr);
-    
 }
 
 struct sockaddr* sockaddr_copy(struct sockaddr* addr) {
     
+    if (addr == NULL || addr->sa_len == 0 || addr->sa_len > sizeof(struct sockaddr_storage))
+        return NULL;
+    
     struct sockaddr* ret = (struct sockaddr*)malloc(addr->sa_len);
-    
+    if (ret == NULL)
+        return NULL;
     memcpy(ret, addr, addr->sa_len);
-    
     return ret;
-    
 }
 
 bool sockaddr_equals(struct sockaddr* addr1, struct sockaddr* addr2) {
     
-    return (addr1->sa_len == addr2->sa_len && memcmp(addr1, addr2, addr1->sa_len) == 0);
+    if (addr1 == NULL || addr2 == NULL || addr1->sa_len == 0 || addr2->sa_len == 0 ||
+        addr1->sa_len > sizeof(struct sockaddr_storage) || addr2->sa_len > sizeof(struct sockaddr_storage))
+        return false;
     
+    return (addr1->sa_len == addr2->sa_len && memcmp(addr1, addr2, addr1->sa_len) == 0);
 }
 
 bool sockaddr_equals_host(struct sockaddr* addr1, struct sockaddr* addr2) {
     
+    if (addr1 == NULL || addr2 == NULL)
+        return false;
+    
     if (addr1->sa_family == addr2->sa_family) {
         if (addr1->sa_family == AF_INET)
             return (memcmp(&((struct sockaddr_in*)addr1)->sin_addr, &((struct sockaddr_in*)addr2)->sin_addr, sizeof(struct in_addr)) == 0);
-        else if (addr1->sa_family == AF_INET6)
+        if (addr1->sa_family == AF_INET6)
             return (memcmp(&((struct sockaddr_in6*)addr1)->sin6_addr, &((struct sockaddr_in6*)addr2)->sin6_addr, sizeof(struct in6_addr)) == 0);
     }
     
-    return 0;
-    
+    return false;
 }
 
 const char* sockaddr_get_host(struct sockaddr* addr) {
     
-    static char ret[1024];
-    
+    static char ret[INET6_ADDRSTRLEN];
     ret[0] = '\0';
     
-    if (addr->sa_family == AF_INET)
-        inet_ntop(AF_INET, &((struct sockaddr_in*)addr)->sin_addr, ret, 1024);
-    else if (addr->sa_family == AF_INET6)
-        inet_ntop(AF_INET6, &((struct sockaddr_in6*)addr)->sin6_addr, ret, 1024);
-    else
+    if (addr == NULL)
         return NULL;
     
-    return ret;
+    const char* result = NULL;
+    if (addr->sa_family == AF_INET)
+        result = inet_ntop(AF_INET, &((struct sockaddr_in*)addr)->sin_addr, ret, sizeof(ret));
+    else if (addr->sa_family == AF_INET6)
+        result = inet_ntop(AF_INET6, &((struct sockaddr_in6*)addr)->sin6_addr, ret, sizeof(ret));
     
+    return result != NULL ? ret : NULL;
 }
 
 uint16_t sockaddr_get_port(struct sockaddr* addr) {
     
+    if (addr == NULL)
+        return 0;
     if (addr->sa_family == AF_INET)
         return ntohs(((struct sockaddr_in*)addr)->sin_port);
-    else if (addr->sa_family == AF_INET6)
+    if (addr->sa_family == AF_INET6)
         return ntohs(((struct sockaddr_in6*)addr)->sin6_port);
-    
     return 0;
-    
 }
 
 void sockaddr_set_port(struct sockaddr* addr, uint16_t new_port) {
     
+    if (addr == NULL)
+        return;
     if (addr->sa_family == AF_INET)
         ((struct sockaddr_in*)addr)->sin_port = htons(new_port);
     else if (addr->sa_family == AF_INET6)
         ((struct sockaddr_in6*)addr)->sin6_port = htons(new_port);
-    
 }
 
 bool sockaddr_is_ipv6(struct sockaddr* addr) {
-    
-    return (addr->sa_family == AF_INET6);
-    
+    return (addr != NULL && addr->sa_family == AF_INET6);
 }
