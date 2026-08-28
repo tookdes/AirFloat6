220da148d041532c61837e4d9d846191267ac985
tookdes <38042623+tookdes@users.noreply.github.com>
2026-08-28 15:11:11 +0800

    fix: make logging stack-safe and thread-safe

--- libairfloat/libairfloat/log.c
+++ libairfloat/libairfloat/log.c
@@ -29,60 +29,58 @@
 //
 
 #include <stdarg.h>
-#include <string.h>
 #include <stdio.h>
-#include <assert.h>
+
+#if defined(LOG_SERVER_FILE)
+#include <pthread.h>
+static pthread_mutex_t write_mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
 
 #include "log.h"
-#include "mutex.h"
-
-#if defined (LOG_SERVER_FILE)
-mutex_p write_mutex = NULL;
-#endif
 
 void log_message(int level, const char* message, ...) {
     
 #if defined(LOG_SERVER) || defined(LOG_SERVER_FILE)
-    assert((level == LOG_INFO || level == LOG_ERROR) && message != NULL);
-    
-    char msgnl[strlen(message) + 2];
-    sprintf(msgnl, "%s\n", message);
+    if ((level != LOG_INFO && level != LOG_ERROR) || message == NULL)
+        return;
     
     va_list args;
     va_start(args, message);
     
 #if defined(LOG_SERVER)
-    vprintf(msgnl, args);
+    vprintf(message, args);
+    fputc('\n', stdout);
 #else
 #if defined(LOG_SERVER_FILE)
-    
-    if (write_mutex == NULL)
-        write_mutex = mutex_create();
-    
-    mutex_lock(write_mutex);
+    pthread_mutex_lock(&write_mutex);
     FILE* log_file = fopen("/var/log/com.tren.AirFloat.log", "a");
-    vfprintf(log_file, msgnl, args);
-    fclose(log_file);
-    mutex_unlock(write_mutex);
-    
+    if (log_file != NULL) {
+        vfprintf(log_file, message, args);
+        fputc('\n', log_file);
+        fclose(log_file);
+    }
+    pthread_mutex_unlock(&write_mutex);
 #endif
 #endif
     
     va_end(args);
+#else
+    (void)level;
+    (void)message;
 #endif
-    
 }
 
 void log_data(int level, const void* data, size_t data_size) {
     
-#if defined (LOG_SERVER)
-    assert((level == LOG_INFO || level == LOG_ERROR) && data != NULL && data_size > 0);
+#if defined(LOG_SERVER)
+    if ((level != LOG_INFO && level != LOG_ERROR) || data == NULL || data_size == 0)
+        return;
     
-    char msgnl[data_size + 3];
-    memcpy(msgnl, data, data_size);
-    memcpy(&msgnl[data_size], "\n", 2);
-    
-    printf("%s", msgnl);
+    fwrite(data, 1, data_size, stdout);
+    fputc('\n', stdout);
+#else
+    (void)level;
+    (void)data;
+    (void)data_size;
 #endif
-    
 }
